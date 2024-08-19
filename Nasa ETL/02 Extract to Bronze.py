020a3df4165228930362c4624548b40ec10f146e
Cavina <Cavina>
2024-08-19 19:54:27 +0000

    Converting Stats Tables to ipynb for dashboard storage

--- Nasa ETL/02 Extract to Bronze.py
+++ Nasa ETL/02 Extract to Bronze.py
@@ -9,11 +9,19 @@ import json
 
 # COMMAND ----------
 
+'''
+Fetches the rover data for the given rover and earth date.
+Accepts the rover, earth_date, and the api_key as parameters.
+Returns 
+'''
+
 def fetch_rover_data(rover, earth_date, api_key):
     params = {"earth_date": earth_date, "api_key": api_key}
     url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rover}/photos?"
     response = requests.get(url, params = params)
     data = response.json()
+    # If no photos are found, return an empty list.
+    # Otherwise this is flattening the first level for us in the JSON.
     photos = data.get('photos', [])
 
     if not photos:
@@ -28,6 +36,11 @@ earth_date = dbutils.widgets.get('earth_date')
 api_key = dbutils.widgets.get('api_key')
 
 # COMMAND ----------
+
+'''
+For every rover in the list, fetch the data for the given earth date.
+Write the data to the bronze directory.
+'''
 
 rovers = ["curiosity", "opportunity", "spirit"]
 
